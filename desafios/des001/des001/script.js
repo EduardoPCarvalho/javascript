dea03341bea1bc85041631e7e248c256de51c859
Eduardo Pimenta de Carvalho <eduardocarvalho.profi@gmail.com>
2025-07-04 16:05:26 -0300

    desafio feito

--- desafios/des001/des001/script.js
+++ desafios/des001/des001/script.js
@@ -1,1 +1,20 @@
-alert('Óla')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        //BOM DIA 
+        img.src = 'imagens/img-manha.png'
+        document.body.style.background = '#ffefc7'
+    } else if (hora >= 12 && hora <= 18) {
+        //BOA TARDE
+        img.src = 'imagens/img-tarde.png'
+        document.body.style.background = '#b9846f'
+    } else {
+        //BOA NOITE
+        img.src = 'imagens/img-noite.png'
+        document.body.style.background = '#05284a'
+    }
+}
